6f0e020bcd6032ef801d344d78b574c18e073e0d
suhao03 <suhao03@58.com>
2020-07-06 21:00:32 +0800

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,20 @@ const uma = Uma.instance({
   //   console.log(umajs, '=======after');
   // },
 });
+const { app } = uma;
 
-console.log(uma.config, '======');
+app.use(async (ctx, next) => {
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, POST, DELETE');
+  ctx.set('Access-Control-Allow-Headers', 'x-requested-with, accept, origin, content-type');
+  ctx.set('Content-Type', 'application/json;charset=utf-8');
+  ctx.set('Access-Control-Allow-Credentials', 'true');
+  if (ctx.method === 'OPTIONS') {
+    ctx.body = '';
+    ctx.status = 204;
+  } else {
+    await next();
+  }
+});
 
 uma.start(8058);
